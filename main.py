4931dc0184301fa53d79a21831d3c3dab0117078
Muhammad Ali <codingprodg@gmail.com>
2025-02-03 23:03:34 +0000

    Basic tkinter setup

---
+++ main.py
@@ -0,0 +1,25 @@
+import tkinter as tk
+
+calculation = ""
+
+def add_to_calculation(symbol):
+    global calculation
+    calculation += str(symbol)
+    text_result.delete(1.0, "end")
+    text_result.insert(1.0, calculation)
+
+def evaluate_calculation():
+    global calculation 
+    try:
+        calculation = str(eval(calculation))
+        text_result.delete(1.0, "end")
+        text_result.insert(1.0, calculation)
+    except:
+        clear_field()
+        text_result.insert(1.0, "Error")
+
+def clear_field():
+    global calculation
+    calculation =""
+    text_result.delete(1.0, "end")
+    pass
